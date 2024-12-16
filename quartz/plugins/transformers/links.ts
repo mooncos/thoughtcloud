663607d29622f52220cc46176f37da479e210d96
bfahrenfort <bfahrenfort1@gmail.com>
2024-12-16 01:05:56 -0600

    lint: format

--- quartz/plugins/transformers/links.ts
+++ quartz/plugins/transformers/links.ts
@@ -86,25 +86,25 @@ export const CrawlLinks: QuartzTransformerPlugin<Partial<Options>> = (userOpts) 
                     refIcon != null
                       ? refIcon
                       : {
-                        type: "element",
-                        tagName: "svg",
-                        properties: {
-                          "aria-hidden": "true",
-                          class: "external-icon",
-                          style: "max-width:0.8em;max-height:0.8em",
-                          viewBox: "0 0 512 512",
-                        },
-                        children: [
-                          {
-                            type: "element",
-                            tagName: "path",
-                            properties: {
-                              d: "M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z",
-                            },
-                            children: [],
+                          type: "element",
+                          tagName: "svg",
+                          properties: {
+                            "aria-hidden": "true",
+                            class: "external-icon",
+                            style: "max-width:0.8em;max-height:0.8em",
+                            viewBox: "0 0 512 512",
                           },
-                        ],
-                      },
+                          children: [
+                            {
+                              type: "element",
+                              tagName: "path",
+                              properties: {
+                                d: "M320 0H288V64h32 82.7L201.4 265.4 178.7 288 224 333.3l22.6-22.6L448 109.3V192v32h64V192 32 0H480 320zM32 32H0V64 480v32H32 456h32V480 352 320H424v32 96H64V96h96 32V32H160 32z",
+                              },
+                              children: [],
+                            },
+                          ],
+                        },
                   )
                 }
 
